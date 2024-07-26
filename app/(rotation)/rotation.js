adf7d162d622e07fb97b4d6204135b5c9025572e
Natalie Lukaschyk <N.Janke@live.de>
2024-07-26 13:57:21 +0200

    bug fixed, add translateY

--- app/(rotation)/rotation.js
+++ app/(rotation)/rotation.js
@@ -1,22 +1,54 @@
-import { View, Text } from "react-native";
-import { Link } from "expo-router";
+import { View, Text, StyleSheet } from "react-native";
 import { globalStyles } from "../../styles/gobalStyles";
 import { DeviceMotion } from "expo-sensors";
 import { useState, useEffect } from "react";
+import { COLORS } from "../../styles/constants";
+import Animated, {
+  useSharedValue,
+  withSpring,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 
 export default function RotationPage() {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
   const [subscription, setSubscription] = useState(null);
+  const [stageDimensions, setStageDimensions] = useState(null);
+  // {width:200, height: 200}
   const [rotation, setRotation] = useState({
     alpha: 0,
     beta: 0,
     gamma: 0,
   });
 
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ],
+    };
+  });
+
   function subscribe() {
     const subscriptionHandle = DeviceMotion.addListener(({ rotation }) => {
-      setRotation(rotation);
+      if (rotation.gamma < -0.2) {
+        translateX.value = withSpring(-stageDimensions.width / 2);
+      } else if (rotation.gamma > 0.2) {
+        translateX.value = withSpring(stageDimensions.width / 2);
+      } else {
+        translateX.value = withSpring(0);
+      }
 
-      console.log(alpha);
+      if (rotation.beta < -0.2) {
+        translateY.value = withSpring(-stageDimensions.height / 2);
+      } else if (rotation.beta > 0.2) {
+        translateY.value = withSpring(stageDimensions.height / 2);
+      } else {
+        translateY.value = withSpring(0);
+      }
+
+      setRotation(rotation);
     });
     setSubscription(subscriptionHandle);
   }
@@ -28,16 +60,55 @@ export default function RotationPage() {
   }
 
   useEffect(() => {
-    subscribe();
+    if (stageDimensions) {
+      subscribe();
+    }
     return () => unsubscribe();
-  }, []);
+  }, [stageDimensions]);
 
   return (
     <>
       <Text style={globalStyles.heading}>Rotation</Text>
-      <Text style={globalStyles.heading}>alpha: {rotation.alpha}</Text>
-      <Text style={globalStyles.heading}>beta: {rotation.beta}</Text>
-      <Text style={globalStyles.heading}>gamma: {rotation.gamma}</Text>
+      <Text style={globalStyles.paragraph}>
+        alpha: {Math.round(rotation.alpha * 10) / 10}
+      </Text>
+      <Text style={globalStyles.paragraph}>
+        beta: {Math.round(rotation.beta * 10) / 10}
+      </Text>
+      <Text style={globalStyles.paragraph}>
+        gamma: {Math.round(rotation.gamma * 10) / 10}
+      </Text>
+      <Text style={globalStyles.paragraph}>
+        Stage Dimension: {stageDimensions?.height} x {stageDimensions?.width}
+      </Text>
+      <View
+        onLayout={({ nativeEvent }) => {
+          console.log(nativeEvent);
+          setStageDimensions({
+            height: nativeEvent.layout.height,
+            width: nativeEvent.layout.width,
+          });
+        }}
+        style={styles.stage}
+      >
+        <Animated.View style={[styles.circle, animatedStyle]} />
+      </View>
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  circle: {
+    position: "relative",
+    width: 60,
+    height: 60,
+    backgroundColor: COLORS.primary,
+    borderRadius: "100%",
+  },
+
+  stage: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
